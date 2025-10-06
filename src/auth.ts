e7e2a088b85f909a52e40397a83789028c1ea60d
Chapman Cheng <45029601+chapman-cc@users.noreply.github.com>
2025-10-06 13:22:49 +0100

    replace postgres connection string

--- src/auth.ts
+++ src/auth.ts
@@ -1,20 +1,14 @@
 import { betterAuth } from "better-auth";
 import { Pool } from "pg";
 import process from "process";
-
-const {
-  DB_USER,
-  DB_PASSWORD,
-  DB_NAME,
-  GITHUB_CLIENT_ID,
-  GITHUB_CLIENT_SECRET,
-} = process.env;
+const { POSTGRES_CONN_STR, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } =
+  process.env;
 
 export const auth = betterAuth({
   database: new Pool({
-    connectionString: `postgres://${DB_USER}:${DB_PASSWORD}@localhost:5432/${DB_NAME}`,
+    connectionString: POSTGRES_CONN_STR,
   }),
-  // emailAndPassword: { enabled: true },
+
   socialProviders: {
     github: {
       clientId: GITHUB_CLIENT_ID,
